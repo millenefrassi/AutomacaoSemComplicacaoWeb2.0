dbc42e421874c27442d26dbea20953ead0304584
Millene <88863741+millenefrassi@users.noreply.github.com>
2023-05-27 20:48:03 -0300

    aula 15 prefixo pages e maps

--- src/test/java/br/com/chronosAcademy/core/Driver.java
+++ src/test/java/br/com/chronosAcademy/core/Driver.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Driver {
-    private WebDriver driver;
+    private static WebDriver driver;
 
     //se alguém quiser usar, precisa inicializar, instanciar...CONSTRUTOR
     public Driver(String navegador){
@@ -38,7 +38,7 @@ public class Driver {
         driver.manage().window().maximize();
     }
 
-    public WebDriver getDriver(){
+    public static WebDriver getDriver(){
         return driver;
     }
 }
