7b39766b2aaf9f312a6285d50a80b61e9c8a75dc
Millene <88863741+millenefrassi@users.noreply.github.com>
2023-11-24 17:05:32 -0300

    aula_27_crud_edit

---
+++ src/test/java/br/com/chronosAcademy/pages/EditAccountPage.java
@@ -0,0 +1,29 @@
+package br.com.chronosAcademy.pages;
+
+import br.com.chronosAcademy.core.Driver;
+import br.com.chronosAcademy.maps.EditAccountMap;
+import org.openqa.selenium.support.PageFactory;
+
+public class EditAccountPage {
+
+    EditAccountMap editAccountMap;
+
+    public EditAccountPage(){
+        editAccountMap = new EditAccountMap();
+        PageFactory.initElements(Driver.getDriver(), editAccountMap);
+    }
+
+    public void setFirstName(String firstName){
+        Driver.visibilityOf(editAccountMap.inpFirstName);
+        editAccountMap.inpFirstName.sendKeys(firstName);
+    }
+
+    public void setLastName(String lastName){
+        Driver.visibilityOf(editAccountMap.inpLastName);
+        editAccountMap.inpLastName.sendKeys(lastName);
+    }
+
+    public void clickBtnSave(){
+        editAccountMap.btnSave.click();
+    }
+}
